09c553a933d5272f25c0bc77b377a9817be5c3e5
Marius Meschter <git@meschter.me>
2024-12-10 23:52:26 +0100

    refactor: remove layout.ts for recipe; not needed anymore

--- src/routes/recipe/[slug]/+page.ts
+++ src/routes/recipe/[slug]/+page.ts
@@ -1,9 +1,21 @@
+import { ApiError } from '$lib/services/apiError';
+import { getRecipeBySlug } from '$lib/services/recipeService';
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ parent }) => {
-    const parentData = await parent();
+export const load: PageLoad = async ({ params, fetch }) => {
+    try {
+        const recipe = await getRecipeBySlug(params.slug, fetch);
 
-    return {
-        recipe: parentData.recipe
-    };
+        return {
+            recipeSlug: params.slug,
+            recipe
+        };
+    } catch (e) {
+        console.error(e);
+        if (e instanceof ApiError) {
+            error(e.statusCode, { message: e.message });
+        }
+        error(500, { message: 'Oops, something went wrong...' });
+    }
 };
