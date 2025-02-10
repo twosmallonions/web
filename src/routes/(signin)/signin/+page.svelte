217d177238068ccb591c06e077d0751b55bbb168
Marius Meschter <git@meschter.me>
2025-02-10 21:20:07 +0100

    chore: format

--- src/routes/(signin)/signin/+page.svelte
+++ src/routes/(signin)/signin/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import "../../../app.css"
-    import { signIn } from "@auth/sveltekit/client"
-    import { onMount } from "svelte";
+    import '../../../app.css';
+    import { signIn } from '@auth/sveltekit/client';
+    import { onMount } from 'svelte';
     onMount(() => {
-        document.getElementById("signin")?.click();
-    })
+        document.getElementById('signin')?.click();
+    });
 </script>
 
 <div class="flex h-full bg-base-100">
-    <button on:click={() => signIn("keycloak", {callbackUrl: ""})} id="signin">Signin</button>
+    <button on:click={() => signIn('keycloak', { callbackUrl: '' })} id="signin">Signin</button>
 </div>
